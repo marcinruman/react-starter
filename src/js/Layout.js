442e0cb42e4227f1b49b5f09e8244e99cdbe5edf
Marcin Ruman <rumanmarcin@gmail.com>
2017-01-31 21:08:24 +0100

    Initial Commit

---
+++ src/js/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>This is a react-starter kit to make your life easier when setting up a new project using React and Webpack.</p>
+            </div>
+        );
+    }
+}
