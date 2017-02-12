1d6724323f34985c56c6cd7fbc6cb892c10e0f64
Marcin Ruman <rumanmarcin@gmail.com>
2017-02-12 19:27:32 +0100

    Consistent Double String Quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
 
 module.exports = {
     context: path.join(__dirname, "src"),
@@ -14,10 +14,10 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 query: {
-                    presets: ['react', 'es2015', 'stage-0'],
-                    plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties', 'transform-react-jsx-img-import']
+                    presets: ["react", "es2015", "stage-0"],
+                    plugins: ["react-html-attrs", "transform-decorators-legacy", "transform-class-properties", "transform-react-jsx-img-import"]
                 }
             }
         ]
